0dfba66d4fa42dd25476eb86cfc791a60e1f1129
G3tech <166132684+n00b01@users.noreply.github.com>
2025-07-10 18:50:40 +0300

    Delete janepher_backend directory
    Deleted obsolete file

--- janepher_backend/app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .user import User
-from .conversation import Conversation
-from .message import Message
--- janepher_backend/app/schemas/__init__.py
+++
@@ -1,3 +0,0 @@
-from .user import UserCreate, UserRead, UserLogin, Token
-from .conversation import ConversationCreate, ConversationRead
-from .message import MessageCreate, MessageRead
